9b3c4f90b9a48fa18c74fd5f31412cc58f131837
hafidhjulian <hafidh6079@gmail.com>
2019-10-02 08:25:07 +0700

    update baru

--- lib/Beranda/berandapage_view.dart
+++ lib/Beranda/berandapage_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:gojekapp/Beranda/beranda_gojek_appbar.dart';
 import 'package:gojekapp/constant.dart';
+import 'package:gojekapp/Beranda/beranda_gojek_appbar.dart';
+import 'package:gojekapp/Beranda/beranda_model.dart';
+import 'dart:async';
+import 'dart:io';
 
 class BerandaPage extends StatefulWidget {
   @override
@@ -8,6 +11,70 @@ class BerandaPage extends StatefulWidget {
 }
 
 class _BerandaPageState extends State<BerandaPage> {
+  List<GojekService> _gojekServiceList = [];
+
+  @override
+  void initState(){
+    super.initState();
+
+    _gojekServiceList.add(new GojekService(
+      image: Icons.directions_bike,
+      color: GojekPalette.menuRide,
+      title: "GO-RIDE"
+    ));
+    _gojekServiceList.add(new GojekService(
+      image: Icons.local_car_wash,
+      color: GojekPalette.menuCar,
+      title: "GO-CAR"
+    ));
+    _gojekServiceList.add(new GojekService(
+      image: Icons.directions_car,
+      color: GojekPalette.menuBluebird,
+      title: "GO-BLUEBIRD"
+    ));
+    _gojekServiceList.add(new GojekService(
+      image: Icons.restaurant,
+      color: GojekPalette.menuFood,
+      title: "GO-FOOD"
+    ));
+    _gojekServiceList.add(new GojekService(
+      image: Icons.next_week,
+      color: GojekPalette.menuSend,
+      title: "GO-SEND"
+    ));
+    _gojekServiceList.add(new GojekService(
+      image: Icons.local_offer,
+      color: GojekPalette.menuDeals,
+      title: "GO-DEALS"
+    ));
+    _gojekServiceList.add(new GojekService(
+      image: Icons.phonelink_ring,
+      color: GojekPalette.menuPulsa,
+      title: "GO-PULSA"
+    ));
+    _gojekServiceList.add(new GojekService(
+      image: Icons.apps,
+      color: GojekPalette.menuOther,
+      title: "LAINNYA"
+    ));
+    _gojekServiceList.add(new GojekService(
+      image: Icons.shopping_basket,
+      color: GojekPalette.menuShop,
+      title: "GO-SHOP"
+    ));
+    _gojekServiceList.add(new GojekService(
+      image: Icons.shopping_cart,
+      color: GojekPalette.menuMart,
+      title: "GO-MART"
+    ));
+    _gojekServiceList.add(new GojekService(
+      image: Icons.local_play,
+      color: GojekPalette.menuTix,
+      title: "GO-TIX"
+    ));
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new SafeArea(
@@ -34,6 +101,25 @@ class _BerandaPageState extends State<BerandaPage> {
       )
     );
   }
+  Widget _buildGojekServiceMenu() {
+  return new SizedBox(
+    width: double.infinity,
+    height: 220.0,
+    child: new Container(
+      margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
+      child: GridView.builder(
+        physics: ClampingScrollPhysics(),
+        itemCount: 8,
+        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+        ),
+        itemBuilder: (context, position){
+          return _rowGojekService(_gojekServiceList[position]);
+        },
+      ),
+    ),
+  );
+}
 }
 
 Widget _buildGopayMenu() {
@@ -152,22 +238,50 @@ Widget _buildGopayMenu() {
   );
 }
 
-Widget _buildGojekServiceMenu() {
-  return new SizedBox(
-    width: double.infinity,
-    height: 220.0,
-    child: new Container(
-      margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
-      child: GridView.builder(
-        physics: ClampingScrollPhysics(),
-        itemCount: 8,
-        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-        ),
-        itemBuilder: (context, position){
-          return new Text("Gojek Menu");
-        },
-      ),
+// Widget _buildGojekServiceMenu() {
+//   return new SizedBox(
+//     width: double.infinity,
+//     height: 220.0,
+//     child: new Container(
+//       margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
+//       child: GridView.builder(
+//         physics: ClampingScrollPhysics(),
+//         itemCount: 8,
+//         gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+//           crossAxisCount: 4,
+//         ),
+//         itemBuilder: (context, position){
+//           return _rowGojekService(_gojekServiceList[position]);
+//         },
+//       ),
+//     ),
+//   );
+// }
+
+Widget _rowGojekService(GojekService gojekService) {
+  return new Container(
+    child: new Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+       new Container(
+         decoration: new BoxDecoration(
+           border: Border.all(color: GojekPalette.grey200, width: 1.0),
+           borderRadius: new BorderRadius.all(new Radius.circular(20.0))
+         ),
+         padding: EdgeInsets.all(12.0),
+         child: new Icon(
+           gojekService.image,
+           color: gojekService.color,
+           size: 32.0,
+         ),
+       ),
+       new Padding(
+         padding: EdgeInsets.only(top: 6.0),
+       ),
+       new Text(
+         gojekService.title, style: new TextStyle(fontSize: 10.0)
+       )
+      ],
     ),
   );
 }
